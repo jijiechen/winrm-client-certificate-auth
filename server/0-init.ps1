b6ae53192a62a38b67c10388f6601a36c8992b36
Jijie Chen <jijie.chen@someplace.com>
2018-05-31 11:03:52 +0800

    Create work path; Fixed typo again

--- server/0-init.ps1
+++ server/0-init.ps1
@@ -15,43 +15,10 @@ Function Init-SecureWinRM(){
     Push-Location
     $time =  (date).ToString("yyMMddHHmmss")
     $path = "$env:TEMP\init-$time"
+    [System.IO.Directory]::CreateDirectory($path)
+
     Set-Location $path
     Write-Host $PublicKey > user.pub
-
-
-
-    Invoke-WebRequest -UseBasicParsing -OutFile enable-secure-winrm.ps1 https://raw.githubusercontent.com/jijiechen/winrm-client-certificate-auth/master/server/enable-secure-winrm.ps1
-    Invoke-WebRequest -UseBasicParsing -OutFile import-client-publickey.ps1 https://raw.githubusercontent.com/jijiechen/winrm-client-certificate-auth/master/server/import-client-publickey.ps1
-
-    # Set-ExecutionPolicy -ExecutionPolicy ByPass
-    .\enable-secure-winrm.ps1
-
-    $credential = New-Object -TypeName PSCredential -ArgumentList $UserName, $(ConvertTo-SecureString $Password -AsPlainText -Force)
-    .\import-client-publickey.ps1 .\user.pub $credential
-
-
-
-    Pop-Location
-    Remove-Item -Recurse -Force $path -ErrorAction SilentlyContinue
-}Function Init-SecureWinRM(){
-    Param
-    (
-         [Parameter(Mandatory=$true, Position=0)]
-         [string] $UserName,
-         [Parameter(Mandatory=$true, Position=1)]
-         [string] $Password,
-         [Parameter(Mandatory=$true, Position=2)]
-         [string] $PublicKey
-    )
-
-    $ErrorActionPreference = "Stop";
-
-    Push-Location
-    $time =  (date).ToString("yyMMddHHmmss")
-    $path = "$env:TEMP\init-$time"
-    Set-Location $path
-    Write-Host $PublicKey > user.pub
-
 
 
     Invoke-WebRequest -UseBasicParsing -OutFile enable-secure-winrm.ps1 https://raw.githubusercontent.com/jijiechen/winrm-client-certificate-auth/master/server/enable-secure-winrm.ps1
