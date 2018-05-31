3d2957371167727f0f6bf3643af3fd52b1742e56
Jijie Chen <jijie.chen@someplace.com>
2018-05-31 21:42:56 +0800

    Show server certificate thumbprint on connecting untrusted server

--- client/connect.ps1
+++ client/connect.ps1
@@ -4,24 +4,70 @@
 
 $private_key_cert_thumbprint = $args[0]
 
+# Enter-PSSession -ComputerName <computer_name> -CertificateThumbprint $private_key_cert_thumbprint -UseSsl -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck)
 
 
 
 Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "*" -Force
 
-# Enter-PSSession -ComputerName <computer_name> -CertificateThumbprint $private_key_cert_thumbprint -UseSsl -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck)
-
-
 Function Connect-RemotePS () {
     Param (
             [Parameter(Mandatory=$true, Position=0)]
             [string] $hostname,
-            [Parameter(Mandatory=$false)]
+            [Parameter(Mandatory=$false, Position=1)]
             [switch] $nocheck
         )
 
-        if($nocheck){
-            $option = New-PSSessionOption -SkipCACheck -SkipCNCheck
+        Function AlreadyTrusted () {
+            $request = [System.Net.HttpWebRequest]::Create("https://$($hostname):5986/")
+            $request.Timeout = 5000
+
+            try { $request.GetResponse().Dispose() }
+            catch [System.Net.WebException]
+            {
+                if ($_.Exception.Status -eq [System.Net.WebExceptionStatus]::TrustFailure)
+                {
+                    $key_data = $request.ServicePoint.Certificate.Export( [Security.Cryptography.X509Certificates.X509ContentType]::Cert )
+
+                    $cert = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2
+                    $cert.Import( $key_data )
+                    
+                    $script_dir = Split-Path $MyInvocation.MyCommand.Path
+                    $trusted_file = "$script_dir\trusted-server-thumbprints"
+                    $trusted = @()
+                    if(Test-Path $trusted_file){
+                        $trusted = [System.IO.File]::ReadAllLines( $trusted_file )
+                    }
+                    
+                    if($trusted.IndexOf($cert.Thumbprint) -gt -1){
+                        Return $true
+                    }
+
+                    Write-Host 'The thumbprint of server certificate is ' -NoNewLine
+                    Write-Host $cert.Thumbprint -ForegroundColor Yellow
+                    $confirmation = Read-Host 'Do you want to trust this server? (Y/N)'
+                    if ( ($confirmation -eq 'Y') -or ($confirmation -eq 'y') ) {
+                        Add-Content -Path $trusted_file -Value $cert.Thumbprint -ErrorAction SilentlyContinue
+                        Return $true
+                    }
+
+                    Return $false
+                }
+                elseif ($_.Exception.Status -eq [System.Net.WebExceptionStatus]::ProtocolError)
+                {
+                    # Connection success
+                }else{
+                    Write-Host "Fail to connect $hostname" -ForegroundColor Red
+                    Throw
+                }
+            }
+
+            Return $false
+        }
+
+
+        if($nocheck -or (AlreadyTrusted)){
+            $option = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
         }else{
             $option = New-PSSessionOption 
         }
